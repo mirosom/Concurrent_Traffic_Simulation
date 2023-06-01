08cc2106b30c8f3d5504c50ebc500b57187e4cea
Miroslav Holec <64981426+mirosom@users.noreply.github.com>
2023-06-01 17:14:14 +0200

    Add files via upload

---
+++ TrafficLight.h
@@ -0,0 +1,69 @@
+#ifndef TRAFFICLIGHT_H
+#define TRAFFICLIGHT_H
+
+#include <mutex>
+#include <deque>
+#include <condition_variable>
+#include "TrafficObject.h"
+
+enum class TrafficLightPhase {red,green};
+
+// forward declarations to avoid include cycle
+class Vehicle;
+
+
+// FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
+// Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
+// Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
+// Also, there should be an std::condition_variable as well as an std::mutex as private members. 
+
+template <class T>
+class MessageQueue
+{
+public:
+    MessageQueue(){}
+    ~MessageQueue(){}
+    T receive();
+    void send(T&&);
+
+private:
+    std::deque<T> _queue;
+    std::mutex _mqMtx;
+    std::condition_variable _mqCV;
+};
+
+// FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
+// The class shall have the public methods „void waitForGreen()“ and „void simulate()“ 
+// as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that 
+// can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
+// Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
+
+class TrafficLight : public TrafficObject
+{
+public:
+    // constructor / desctructor
+    // getters / setters
+    // typical behaviour methods
+    TrafficLight();
+    ~TrafficLight(){}
+    void waitForGreen();
+    void simulate();
+    TrafficLightPhase getCurrentPhase(); 
+
+private:
+
+    void cycleThroughPhases();
+    // typical behaviour methods
+
+    // FP.4b : create a private member of type MessageQueue 
+    // for messages of type TrafficLightPhase 
+    // and use it within the infinite loop to push each 
+    // new TrafficLightPhase into it by calling 
+    // send in conjunction with move semantics.
+    TrafficLightPhase _currentPhase;
+    MessageQueue<TrafficLightPhase> _message;
+    std::condition_variable _condition;
+    std::mutex _mutex;
+};
+
+#endif
